b98606b0f24aa0db3935c68be369271284dada96
MettaCode <mettacode@gmail.com>
2024-04-18 02:26:52 -0500

    onesignal generalization and web flow updates

--- push/onesignal/OneSignalSDKWorker.js
+++ push/onesignal/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-//! KEEP Until YYYY-MM-DD: Required for pushes to work correctly for users who have not re-visited to migrate to the new OneSignal specific ServiceWorker.
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-// // And some other site specific code in the same file example.
-// importScripts('https://apparel.scapegoatsusa.com/flutter_service_worker.js');
